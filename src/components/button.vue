e3743a7bfdcea8426adbc2163c7eb48135d045a4
ZilvinasBaltrunas <zilvino@hotmail.com>
2018-08-27 19:39:31 +0300

    inserted v-model

---
+++ src/components/button.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <button class="button">
+            <slot></slot>
+        </button>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .button {
+        background: #4169E1;
+        color: #fff;
+        border-radius: 5px;
+        padding-left: 1rem;
+        padding-right: 1rem;
+        padding-bottom: 0.5rem;
+        padding-top: 0.5rem;
+    }
+</style>
--- src/pages/contacts.vue
+++ src/pages/contacts.vue
@@ -1,9 +1,37 @@
 <template>
-    <div>
+    <div class="form">
         KONTAKTAI
         <p>Vardas</p>
-        <input type="text">
+        <input v-model="form.name" type="text">
         <p>Zinute</p>
-        <input type="text">
+        <textarea v-model="form.message" type="text"/>
+        <AppButton @click.native="getInfo">SIUSTI</AppButton>
+        <p>{{form}}</p>
     </div>
 </template>
+
+<script>
+import AppButton from '../components/button'
+    export default {
+        name: 'contacts',
+        components: {
+            AppButton
+        },
+        methods: {
+            getInfo () {
+                console.log(this.form)
+            }
+        },
+        data () {
+            return {
+                form: {}
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .form {
+        margin-bottom: 1rem;
+    }
+</style>
